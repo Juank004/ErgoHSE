f13c2eeaaeeda17f52cbf630d52c300f5040317d
Juank004 <juancordova_004@hotmail.com>
2026-06-18 20:26:52 -0500

    Configurada base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/ErgoHSE/'
+  plugins: [react()],
+  base: '/ErgoHSE/' // <-- IMPORTANTE: nombre de tu repositorio
 })
